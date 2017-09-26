9da8baa7c79b345bea66ea3ed9b097ef2e656275
Mech Mech <mecode717@gmail.com>
2017-09-26 16:41:31 +0700

    Complete seven actions

--- app/controllers/lipsticks_controller.rb
+++ app/controllers/lipsticks_controller.rb
@@ -15,6 +15,26 @@ class LipsticksController < ApplicationController
       render 'new'
     end
   end
+  def show
+    @lipstick = Lipstick.find(params[:id])
+  end
+  def edit
+    @lipstick = Lipstick.find(params[:id])
+  end
+  def update
+    @lipstick = Lipstick.find(params[:id])
+    if @lipstick.update(lipstick_params)
+      redirect_to lipsticks_path
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @lipstick = Lipstick.find(params[:id])
+    @lipstick.destroy
+    redirect_to lipsticks_path
+  end
+
   private
   def lipstick_params
     params.require(:lipstick).permit(:name, :brand, :is_best_seller, :description, :price, :image)
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get 'lipsticks', to: 'lipsticks#index'
   get 'lipsticks/new', to: 'lipsticks#new', as: 'new_lipstick'
   post 'lipsticks', to: 'lipsticks#create'
+  get 'lipsticks/:id', to: 'lipsticks#show', as: 'lipstick'
+  get 'lipsticks/:id/edit', to: 'lipsticks#edit', as: 'edit_lipstick'
+  put 'lipsticks/:id', to: 'lipsticks#update'
+  patch 'lipsticks/:id', to: 'lipsticks#update'
+  delete 'lipsticks/:id', to: 'lipsticks#destroy'
 end
