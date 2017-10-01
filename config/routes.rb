4f149c0a0cbb87a74d6e6c57476e0f7df04561f9
Mech Mech <mecode717@gmail.com>
2017-10-01 21:25:46 +0700

    login - signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
   get 'lipsticks', to: 'lipsticks#index'
   get 'lipsticks/new', to: 'lipsticks#new', as: 'new_lipstick'
   post 'lipsticks', to: 'lipsticks#create'
